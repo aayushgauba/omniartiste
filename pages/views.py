bcded2181193bc4b09d3125cd08b9ac1a037265a
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2024-09-05 22:38:09 -0500

    Sitemap

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, "index.html")
 
 def about(request):
     return render(request, "about.html")
+
+def sitemap(request):
+    return HttpResponse(open('templates/sitemap.xml').read(), content_type='text/xml')
